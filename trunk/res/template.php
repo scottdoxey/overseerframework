b814d66b23e81e3f754234a986e81972a5723d1e
neogeek0101 <neogeek0101@d87d2f4a-3e43-0410-a3c2-5d20fac9b018>
2008-03-08 11:43:30 +0000

    Changed site template variables from %HEADER% to %HEAD% and %CONTENT% to %BODY%. Updated and completed template section of documentation. Update documentation styles.

--- trunk/res/template.php
+++ trunk/res/template.php
@@ -17,11 +17,12 @@ function ob_template($buffer) {
 	preg_match('/<title>(.*)<\/title>/si', $buffer, $regs);
 	$ob_template = str_replace('%TITLE%', trim($regs[1]), $ob_template);
 
-	preg_match('/<\/title>(.*)<\/head>/si', $buffer, $regs);
-	$ob_template = str_replace('%HEADER%', trim($regs[1]), $ob_template);
+	preg_match('/<head>(.*)<\/head>/si', $buffer, $regs);
+	$regs[1] = preg_replace('/<title>(.*)<\/title>/si', $regs[1]);
+	$ob_template = str_replace('%HEAD%', trim($regs[1]), $ob_template);
 
 	preg_match('/<body>(.*)(<\/body>)/si', $buffer, $regs);
-	$ob_template = str_replace('%CONTENT%', trim($regs[1]), $ob_template);
+	$ob_template = str_replace('%BODY%', trim($regs[1]), $ob_template);
 
 	// $ob_template = ereg_replace('%+[[:alnum:]_]+%', '', $ob_template);
 
